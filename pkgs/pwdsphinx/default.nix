c376f81cb893575a54d20a240a5dd354782c1602
Rolf Schröder <rolf.schr@gmail.com>
2021-03-17 14:50:34 +0100

    Improve pwdsphinx/default.nix

---
+++ pkgs/pwdsphinx/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, buildPythonPackage, src, version, libsphinx, pysodium, securestring }:
+with pkgs;
+
+buildPythonPackage rec {
+  pname = "pwdsphinx";
+  inherit src version;
+
+  propagatedBuildInputs = [ libsodium libsphinx pysodium securestring ];
+
+  postPatch = let soext = stdenv.hostPlatform.extensions.sharedLibrary;
+  in ''
+    substituteInPlace pwdsphinx/sphinxlib.py \
+      --replace "ctypes.util.find_library('sphinx') or ctypes.util.find_library('libsphinx')" "'${libsphinx}/lib/libsphinx${soext}'"
+  '';
+
+  # todo: add checks with self-signed cert
+  doCheck = false;
+}
